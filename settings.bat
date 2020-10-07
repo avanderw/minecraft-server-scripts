ef3500287e3f2e02edf88db1d3f9acd89bbcea6a
Andrew van der Westhuizen <avanderw@gmail.com>
2020-10-07 21:23:56 +0200

    SAVEPOINT

---
+++ settings.bat
@@ -0,0 +1,23 @@
+SET MAX_RAM=5G
+SET GAME_ARGS=nogui
+SET JAVA_ARGS=-d64 -server -XX:+AggressiveOpts -XX:ParallelGCThreads=3 -XX:+UseConcMarkSweepGC -XX:+UnlockExperimentalVMOptions -XX:+UseParNewGC -XX:+ExplicitGCInvokesConcurrent -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=50 -XX:+UseFastAccessorMethods -XX:+OptimizeStringConcat -XX:NewSize=84m -XX:+UseAdaptiveGCBoundary -XX:NewRatio=3 -Dfml.readTimeout=90 -Dfml.queryResult=confirm
+
+SET CRASH_COUNT=5
+SET CRASH_TIMER=600
+
+SET JAVA_PATH=DISABLE
+SET RUN_FROM_BAD_FOLDER=0
+SET IGNORE_OFFLINE=0
+SET IGNORE_JAVA_CHECK=0
+SET USE_SPONGE=0
+SET HIGH_CPU_PRIORITY=0
+
+SET JMX_ENABLE=0
+SET JMX_PORT=9010
+SET JMX_HOST=127.0.0.1
+
+SET MODPACK_NAME=All The Mods 3 Remix
+SET DEFAULT_WORLD_TYPE=BIOMESOP
+SET MCVER=1.12.2
+SET FORGEVER=14.23.5.2811
+SET FORGEURL=DISABLE
